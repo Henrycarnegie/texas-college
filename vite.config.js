c00cd90e09db9bfdaa28f77afee593fcafb7218f
Henry Carnegie <henrycarnegie23@gmail.com>
2024-12-15 14:38:52 +0700

    FE

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite'
 import laravel from 'laravel-vite-plugin'
 
 export default defineConfig({
-    server: {
-        port: process.env.PORT || 3000,
-    },
     plugins: [
         laravel({
             input: ['resources/sass/app.scss', 'resources/js/app.js'],
